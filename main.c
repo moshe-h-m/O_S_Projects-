f5e912f1d34edb40ad60849396a75cbd2aa2b6c5
moshemak <mhm23811@gmail.com>
2022-04-25 22:29:35 +0300

    this is with the argoment pasing

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
-int main(){
-	printf("hello world\n");
-	for(int i=0;i<14;i++){
-		printf("*");
+int main( int argc, char* argv[]){
+	int i;
+	printf("the number of argument is: %d\n",argc);
+	for(i=1;i<argc+1;i++){
+		printf("the %d argument numbers is %s\n",i,argv[i]);
 		}
-	printf("\n");
-	return 0;
+
 }
-
-
-
