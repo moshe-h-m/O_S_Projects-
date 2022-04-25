14371d8126520b0c72915659da13a619a36b59f8
moshemak <mhm23811@gmail.com>
2022-04-25 21:45:04 +0300

    now its contain only hello world

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 
-int main( int argc, char* argv[]){
-	int i;
-	printf("the number of argument is: %d\n",argc);
-	for(i=1;i<argc+1;i++){
-		printf("the %d argument numbers is %s\n",i,argv[i]);
-		}
-
+int main(){
+	printf("hello world\n");
+	return 0;
 }
+
+
+
